16e55d0eba556d259f95fe75f517e815af4027f4
YannQuillard <y38.quillard@gmail.com>
2021-10-24 19:18:00 +0200

    🔧 database configuration

--- config/container.php
+++ config/container.php
@@ -1,7 +1,20 @@
 <?php
-
+use Psr\Container\ContainerInterface;
 use Slim\App;
 
-return function (App $app) {
+return function (ContainerInterface $container) {
     //empty
+    $container->set('PDO', function() {
+        $host = $_ENV['DB_HOST'];
+        $name = $_ENV['DB_DATABASE'];
+        $username = $_ENV['DB_USERNAME'];
+        $password = $_ENV['DB_PASSWORD'];
+        $flags = [
+            PDO::ATTR_PERSISTENT => false,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci'
+        ];
+        
+        return new PDO("mysql:host=$host;dbname=$name", $username, $password, $flags);
+    });
 };
